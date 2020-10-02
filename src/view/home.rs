e9aa399a62432c4a7f6aa6294cd99acea6cb749c
Nick Senger <nicholas.senger@synthego.com>
2020-10-01 18:38:32 -0700

    feat: finish auth views

--- src/view/home.rs
+++ src/view/home.rs
@@ -1,12 +1,7 @@
 use seed::{prelude::*, *};
 use seed_style::{pc, px, *};
 
-use crate::{
-    BASE_URI,
-    components::link,
-    messages::Msg,
-    state::Model,
-};
+use crate::{components::link, messages::Msg, state::Model, BASE_URI};
 
 pub fn view(_model: &Model) -> Node<Msg> {
     div![
@@ -38,100 +33,77 @@ pub fn view(_model: &Model) -> Node<Msg> {
             div![
                 s().media("@media screen and (max-width: 768px)").flex_direction("column"),
                 s().display("flex"),
-                div![
-                    s().flex_basis("100%"),
-                    s().max_width("100%"),
-                    s().media("@media only screen and (min-width: 768px)").flex_basis("33.33333333%"),
-                    s().media("@media only screen and (min-width: 768px)").max_width("33.33333333%"),
-                    s().padding_right(px(16)),
-                    s().padding_left(px(16)),
-                    h3![
-                        attrs! { At::Class => "spectrum-Heading spectrum-Heading--S" },
-                        "Efficiency"
+                home_section(
+                    "Efficiency",
+                    vec![
+                        "Total Recall uses the SuperMemo-2 algorithm to determine if and when each of your vocabulary",
+                        " flashcards should be reviewed.",
+                        " Reviewing flashcards at calculated intervals means that you",
+                        " will lean more, faster."
                     ],
-                    br![],
-                    div![
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            "Total Recall uses the SuperMemo-2 algorithm to determine if and when each of your vocabulary",
-                            " flashcards should be reviewed.", " Reviewing flashcards at calculated intervals means that you",
-                            " will lean more, faster."
-                        ],
+                    vec![
+                        link(
+                            "Read more about SuperMemo",
+                            "https://en.wikipedia.org/wiki/SuperMemo"
+                        )
+                    ]
+                ),
+                home_section(
+                    "Automation",
+                    vec![
+                        "Images and audio are downloaded for each and every flashcard added to Total Recall,",
+                        " so you can spend less time configuring and more time studying."
+                    ],
+                    vec![
+                        link(
+                            "View supported languages",
+                            "https://translate.google.com/about/languages/"
+                        )
+                    ]
+                ),
+                home_section(
+                    "Flexibility",
+                    vec![
+                        "Applications are available for web and Android (not iOS because they charge to target their platform)",
+                        ", and a lightning-fast GraphQL API written in Rust is exposed to support custom client implementations."
+                    ],
+                    vec![
+                        link(
+                            "Download the Android client",
+                            format!("{}/trc.apk", BASE_URI).as_str(),
+                        ),
                         br![],
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            link(
-                                "Read more about SuperMemo",
-                                "https://en.wikipedia.org/wiki/SuperMemo"
-                            )
-                        ],
-                    ],
-                    br![],
-                ],
-                div![
-                    s().flex_basis("100%"),
-                    s().max_width("100%"),
-                    s().media("@media only screen and (min-width: 768px)").flex_basis("33.33333333%"),
-                    s().media("@media only screen and (min-width: 768px)").max_width("33.33333333%"),
-                    s().padding_right(px(16)),
-                    s().padding_left(px(16)),
-                    h3![
-                        attrs! { At::Class => "spectrum-Heading spectrum-Heading--S" },
-                        "Automation"
-                    ],
-                    br![],
-                    div![
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            "Images and audio are downloaded for each and every flashcard added to Total Recall,",
-                            " so you can spend less time configuring and more time studying."
-                        ],
-                        br![],
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            link(
-                                "View supported languages",
-                                "https://translate.google.com/about/languages/"
-                            )
-                        ],
-                    ],
-                    br![],
-                ],
-                div![
-                    s().flex_basis("100%"),
-                    s().max_width("100%"),
-                    s().media("@media only screen and (min-width: 768px)").flex_basis("33.33333333%"),
-                    s().media("@media only screen and (min-width: 768px)").max_width("33.33333333%"),
-                    s().padding_right(px(16)),
-                    s().padding_left(px(16)),
-                    h3![
-                        attrs! { At::Class => "spectrum-Heading spectrum-Heading--S" },
-                        "Flexibility"
-                    ],
-                    br![],
-                    div![
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            "Applications are available for web and Android (not iOS because they charge to target their platform)",
-                            ", and a lightning-fast GraphQL API written in Rust is exposed to support custom client implementations."
-                        ],
-                        br![],
-                        p![
-                            attrs! { At::Class => "spectrum-Body--M" },
-                            link(
-                                "Download the Android client",
-                                format!("{}/trc.apk", BASE_URI).as_str(),
-                            ),
-                            br![],
-                            link(
-                                "Explore the API",
-                                format!("{}/graphiql", BASE_URI).as_str(),
-                            )
-                        ],
-                    ],
-                    br![],
-                ]
+                        link(
+                            "Explore the API",
+                            format!("{}/graphiql", BASE_URI).as_str(),
+                        )
+                    ]
+                )
             ]
         ]
+    ]
+}
+
+fn home_section(title: &str, description: Vec<&str>, links: Vec<Node<Msg>>) -> Node<Msg> {
+    div![
+        s().flex_basis("100%"),
+        s().max_width("100%"),
+        s().media("@media only screen and (min-width: 768px)")
+            .flex_basis("33.33333333%"),
+        s().media("@media only screen and (min-width: 768px)")
+            .max_width("33.33333333%"),
+        s().padding_right(px(16)),
+        s().padding_left(px(16)),
+        h3![
+            attrs! { At::Class => "spectrum-Heading spectrum-Heading--S" },
+            title
+        ],
+        br![],
+        div![
+            p![attrs! { At::Class => "spectrum-Body--M" }, description],
+            br![],
+            p![attrs! { At::Class => "spectrum-Body--M" }, links],
+        ],
+        br![],
     ]
 }
